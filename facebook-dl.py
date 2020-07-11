b5490f2793b64f42a687829c03f9f8387411bc01
Colin F MacKenzie <colin.1.mackenzie@nokia.com>
2020-07-11 16:12:25 -0400

    Updated for changes to facebook, fb no longer uses sd_src and hd_src. Also refactored code to be a class. Added support for private videos by storing facebook auth cookies in a user file

--- facebook-dl.py
+++ facebook-dl.py
@@ -11,6 +11,7 @@
 
 import re
 import sys
+import os
 import argparse
 import requests
 
@@ -20,6 +21,106 @@ import requests
 # "Downloading..." was.
 ERASE_LINE = '\e[2K'
 
+class FaceBookVideo:
+    cookies: dict
+    meta: dict = {}
+    url: str
+    type: str
+    width: int
+    height: int
+
+    outputFilename: str
+
+    def __init__(self, cookies: dict):
+        self.cookies = cookies
+
+    # parse a collection of videos for all video links
+    def videosBy(self, url: str):
+        # request the index for this video
+        request = requests.get(url, cookies=self.cookies)
+
+        # parse index metadata
+        videos = {}
+        links = re.findall(r"<a\s+href=\"([^\"]*)\"\s+aria-label=\"([^\"]*)\"", request.text)
+        for link in links:
+            # capture 0 is the meta property name and 1 is the property value
+            videos[link[0]] = link[1]
+
+        return videos
+
+    def query(self, url: str):
+        # clear video specific members
+        meta = {}
+        type = None
+        width = None
+        height = None
+
+        if url[0]=='/':
+            url = "https://www.facebook.com" + url;
+
+        # request the index for this video
+        request = requests.get(url, cookies=self.cookies)
+
+        # parse index metadata
+        meta = re.findall(r"<meta\s+property=\"([^\"]*)\"\s+content=\"([^\"]*)\"\s*/>", request.text)
+        for prop in meta:
+            # capture 0 is the meta property name and 1 is the property value
+            self.meta[prop[0]] = prop[1]
+
+        # determine what the video url is from some possibilities
+        # we do it with if/else so we can keep priority
+        if 'og:video' in self.meta:
+            self.url = self.meta['og:video']
+        elif 'og:video:url' in self.meta:
+            self.url = self.meta['og:video:url']
+        elif 'og:video:secure_url' in self.meta:
+            self.url = self.meta['og:video:secure_url']
+        else:
+            print("Cannot determine the video URL from meta: ", self.meta)
+            return False
+
+        # parse other metadata we want
+        for k, v in self.meta.items():
+            if k == 'og:video:width':
+                self.width = v
+            if k == 'og:video:height':
+                self.height = v
+            if k == 'og:video:type':
+                self.type = v
+
+        # now convert the &amp; in the URL
+        self.url = self.url.replace('&amp;', '&')
+
+        # build an output filename
+        base_fn = str(re.findall(r"videos/([^?\"]+)", request.text)[-1].replace("/", "").replace(".","-"))
+        self.outputFilename = f"{base_fn}x{self.height}.mp4"
+
+        return True
+
+    def download(self, outputFilename=None):
+        if outputFilename:
+            # use filename given to us
+            self.outputFilename = outputFilename
+
+        # fn = re.findall(f"{'sd_src' if args.resolution == 'sd' else 'hd_src'}:\"(.+?)\"", request.text)[0]
+        request = requests.get(self.url, cookies=self.cookies)
+
+        # Write the content to the file
+        with open(self.outputFilename, "wb") as f:
+            f.write(request.content)
+
+
+# parse cookies as semi-colon separated string
+# This is what we pull from Chrome network inspector (Request Headers cookies value) if the videos are not public
+# and require a login to access. (You must login under an account that can view the videos and copy your cookies)
+def parseCookies(cookiesText: str):
+    cookies_parsed = re.findall(r"([a-zA-Z\-_]+)=([^;]*);", cookiesText)
+    cookies = {}
+    for c in cookies_parsed:
+        cookies[c[0]] = c[1]
+    return cookies
+
+
 def main():
     parser = argparse.ArgumentParser(description="Download videos from facebook from your terminal")
 
@@ -28,37 +129,46 @@ def main():
 
     args = parser.parse_args()
 
-    print("Fetching source code...", end="\r", flush=True)
-    request = requests.get(args.url)
-    print(ERASE_LINE, end="\r", flush=True)
+    # Videos marked private may require you to be logged into your account
+    # You can use your browser's Network Inspector or Cookies tab to extract your logged in authentication tokens
+    # stored in the 'c_user' and 'xs' variables and store in a file as name-value (w/semicolon):
+    # c_user=<value>;
+    # xs=<value>;
+    # You can copy all your cookies in there, it doesnt matter, but only these two should be required.
+    cookiefile = None
+    if os.path.isfile("cookies"):
+        cookiefile = "cookies"
+    elif os.path.isfile(os.path.expanduser("~/.facebook-cookies")):
+        cookiefile = os.path.expanduser("~/.facebook-cookies")
 
-    print("\033[92m✔\033[0m Fetched source code")
+    cookies = None
+    if cookiefile:
+        # read the cookies and parse
+        print("using facebook cookies from file", cookiefile)
+        with open(cookiefile, "r") as f:
+            cookies = parseCookies(f.read())
 
-    # Create the file name by extracting the video ID from html and then add
-    # "hd" or "sd" depending on the quality of the resolution that is being downloaded.
-    #
-    # To decide whether to use "hd or "sd" we are using an if-then-else
-    # one-liner statement. It might look a little confusing at first, but it
-    # makes a lot of sense once you get an understanding of it.
-    # Read this if you are a little confused: https://stackoverflow.com/a/2802748/9215267
-    file_name = str(re.findall(r"videos\/(.+?)\"", request.text)[-1].replace("/", "")) + f"_{'sd' if args.resolution == 'sd' else 'hd'}.mp4"
+    # create video object of our desired Video
+    video = FaceBookVideo(cookies)
 
-    print("Downloading video...", end="\r", flush=True)
+    if 'videos_by' in args.url:
+        urls = video.videosBy(args.url)
+    else:
+        urls = {args.url: ''}
 
-    try:
-        request = requests.get(re.findall(f"{'sd_src' if args.resolution == 'sd' else 'hd_src'}:\"(.+?)\"", request.text)[0])
-    except IndexError:
-        print(ERASE_LINE, end="\r", flush=True)
-        print("\e[91m✘\e[0m Video could not be downloaded")
-        sys.exit()
+    for url,label in urls.items():
+        print(f"{url}:     {label}")
 
-    # Write the content to the file
-    with open(file_name, "wb") as f:
-        f.write(request.content)
+        if video.query(url):
+            print("   \033[92m✔\033[0m Fetched video metadata ")
 
-    print(ERASE_LINE, end="\r", flush=True)
+            try:
+                video.download()
+            except IndexError as ie:
+                print("   Video could not be downloaded: " + str(ie))
+                sys.exit()
 
-    print(f"\033[92m✔\033[0m Video downloaded: {file_name}")
+            print(f"   \033[92m✔\033[0m Downloaded video {video.outputFilename}")
 
 if __name__ == "__main__":
     main()
